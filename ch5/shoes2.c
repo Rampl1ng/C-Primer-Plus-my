c559597d72da0a62400055c78c9e51348ff784e6
Rampl1ng <rampling222@163.com>
2022-05-29 15:55:11 +0800

    ch5

--- ch5/shoes2.c
+++ ch5/shoes2.c
@@ -7,14 +7,15 @@ int main(void)
     double shoe, foot;
 
     printf("Shoe size (men's) foot length\n");
-    shoe = 3.0;
-    while (shoe < 18.5) /* starting the while loop */
+
+    shoe = 2.0;
+    while (++shoe < 18.5) /* starting the while loop */
     {
         /* start of block */
         foot = SCALE * shoe + ADJUST;
         printf("%10.1f %15.2f inches\n", shoe, foot);
-        shoe = shoe + 1.0;
     }
+
     printf("If the shoe fits, wear it.\n");
 
     return 0;
